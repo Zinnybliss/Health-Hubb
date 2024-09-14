2c3a40a63b4c9d4d280e39dd84697d0d2bb0920a
unknown <chiwueze.ezinne@gmail.com>
2024-09-14 15:24:21 +0100

    made minor change in the footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,17 +1,23 @@
+import logoPic from '../../images/img1.png';
+import { BsEnvelopePaper } from "react-icons/bs";
+import { BiPhone } from "react-icons/bi";
+
+
 const Footer = () => {
   return (
     <footer className='footer w-screen'>
-      <div>
-        <img />
-        <p>
+      <div className=' flex gap-10 text-white h-full w-full items-center justify-center'>
+      <div className='sect1 w-60'>
+        <img src={logoPic} className='size-14 '/>
+        <p className='text-wrap'>
           Lorem ipsum dolor sit amet consectetur. Mattis quis nibh elit eu
           lectus sollicitudin gravida lorem.
         </p>
         <h3>Contact Us</h3>
-        <p>xxxx xxx xxxx</p>
-        <p>Dolorsitamet@gmail.com</p>
+        <p className='flex items-center gap-3'><BiPhone />xxxx xxx xxxx</p>
+        <p className='flex items-center gap-3'><BsEnvelopePaper />Dolorsitamet@gmail.com</p>
       </div>
-      <div>
+      <div className='sect2'>
         <h3>Information</h3>
         <h4>About Us</h4>
         <h4>Features</h4>
@@ -20,7 +26,7 @@ const Footer = () => {
         <h4>Vaccine Hub</h4>
         <h4>Testimonials</h4>
       </div>
-      <div>
+      <div className='sect3'>
         <h3>Helpful Links</h3>
         <p>Services</p>
         <p>Help Center</p>
@@ -28,13 +34,18 @@ const Footer = () => {
         <p>Terms and Condition</p>
         <p>Privacy Policy</p>
       </div>
-      <div>
-        <h3></h3>
-        <p></p>
+      <div className='sect4 '>
+        <h3>Newsletter</h3>
+        <p className='text-wrap'>Lorem ipsum dolor sit amet consectetur, Ac in.</p>
+        <div className='subsect4 flex flex-col gap-4'>
         <input type='email' />
-        <input type='submit' />
+        <button className='bg-[#2D3142;]'>Submit</button> 
+        </div>
       </div>
-      {/* <hr></hr> */}
+      </div>
+      <div>
+      <hr/>
+      </div>
     </footer>
   );
 };
